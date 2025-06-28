697c501ce78c9c3463942272073e76eb560445fc
Francis Iboko <eyanaefrancis2001@gmail.com>
2025-06-28 23:26:21 +0400

    initialized routes to our pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import {Route, Routes} from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/movies' element = {<Movies/>}/>
+        <Route path='/movies/:id' element = {<MovieDetails/>}/>
+        <Route path='/movies/:id/:date' element ={<SeatLayout/>}/>
+        <Route path='/my-bookings' element = {<MyBookings/>}/>
+        <Route path='/favorite' element = {<Favorite/>}/>
+      </Routes>
+    </>
   )
 }
 
