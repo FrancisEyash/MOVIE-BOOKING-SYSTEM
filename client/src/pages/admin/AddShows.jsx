2d776d59c0492bf1119b8cea5c3a77e28a284cd8
Francis Iboko <eyanaefrancis2001@gmail.com>
2025-06-30 18:36:32 +0400

    done with adminNavbar

---
+++ client/src/pages/admin/AddShows.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddShows = () => {
+  return (
+    <div>
+      <h1>addShows</h1>
+    </div>
+  )
+}
+
+export default AddShows
---
+++ client/src/pages/admin/ListShows.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListShows = () => {
+  return (
+    <div>
+      <h1>ListShows</h1>
+    </div>
+  )
+}
+
+export default ListShows
