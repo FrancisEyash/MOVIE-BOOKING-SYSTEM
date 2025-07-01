f7a0797c932983dfaed8caf6c8f8c2b1b2ea5476
Francis Iboko <eyanaefrancis2001@gmail.com>
2025-07-01 05:20:16 +0400

    connected our app with mongodb

---
+++ server/configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connecdDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database connected")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connecdDB;
