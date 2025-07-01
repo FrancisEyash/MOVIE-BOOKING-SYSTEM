53f38cd15b226fc91c5f8681e5af10d50f39afa3
Francis Iboko <eyanaefrancis2001@gmail.com>
2025-07-01 19:27:26 +0400

    can get nowPlaying movies

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connecdDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -20,6 +21,7 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("Server is Live!"));
 // Set up the "/api/inngest" (recommended) routes with the serve handler
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/show", showRouter);
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
